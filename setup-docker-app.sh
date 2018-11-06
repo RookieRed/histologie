15260111577e1508043ee5c0408a98013740c1c7
RookieRed <cedric.eloundou-ngah@niji.fr>
2018-11-06 13:31:16 +0100

    Création d'un script d'installation

---
+++ setup-docker-app.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+docker-compose stop;
+docker-compose build php;
+docker-compose run php /bin/bash -c "mkdir -p /web/logs /web/commande/pdf; \
+        chown -R www-data:www-data /web/logs /web/commande/pdf; \
+        cd /web && composer install;";
+docker-compose up -d;
+
+if [ "$#" -gt 0 ] &&  [ -f $1 ]; then
+    mysql_base_cmd='mysql -u "$MYSQL_USER" --database="$MYSQL_DATABASE" --password="$MYSQL_PASSWORD"';
+    echo "Waiting for mysql service to be ready...";
+    sleep 2m;
+    docker-compose exec mysql sh -c "$mysql_base_cmd < /web/$1";
+    echo "Import from sql file completed !";
+fi
